e9dc59044aa35092dc2c08a365ff942a87a803cd
Christopher Arias <chrstphr.aaron@gmail.com>
2020-07-19 23:58:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function sayHiToGrandma(string) {
   var cantHear = "I cant' hear you!";
   var yesIndeed = "YES INDEED!";
   var loveYou = "I love you, too.";
-  if (string.)
+  if (string.toLowerCase(string))
 }
